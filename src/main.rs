a9c9e3edeb942d1026a0a0c22003a1cfea47a47b
Alexey Kotlyarov <a@koterpillar.com>
2019-09-05 16:08:43 +1000

    Switch to Match structure

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{Read, stdin};
 use std::iter::Iterator;
 
-fn grep(pattern: &str, file: &mut Read, action: &Fn(&str) -> ()) {
+fn grep(pattern: &str, file: &mut dyn Read, action: &dyn FnMut(Match) -> ()) {
 
     let mut contents = String::new();
 
@@ -12,12 +12,29 @@ fn grep(pattern: &str, file: &mut Read, action: &Fn(&str) -> ()) {
 
     contents
         .lines()
-        .filter(move |l| l.contains(pattern))
+        .flat_map(|l| matches(l, pattern))
         .for_each(action)
 }
 
-fn print_match(match_: &str) {
-    println!("{}", match_)
+#[derive(Debug, PartialEq, Eq)]
+struct Match {
+    line: String,
+    hit: (u32, u32), // starts and ends of each match
+    // FIXME multiple hits in the same line
+}
+
+fn matches(line: &str, pattern: &str) -> Option<Match> {
+    match line.find(pattern) {
+        Some(start) => Some(Match{
+            line: line.to_string(),
+            hit: (start as u32, (start + pattern.len()) as u32),
+        }),
+        None => None,
+    }
+}
+
+fn print_match(match_: Match) {
+    println!("{}", match_.line)
 }
 
 fn main() {
@@ -31,5 +48,40 @@ fn main() {
     } else {
         panic!("wrong number of arguments")
     }
-
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_matches() {
+        assert_eq!(matches("hello world", "earth"), None);
+        assert_eq!(matches("hello world", "world"), Some(Match {
+            line: String::from("hello world"),
+            hit: (6, 11),
+        }));
+    }
+
+    #[test]
+    fn test_grep() {
+        let mut results: Vec<Match> = vec![];
+
+        let mut file = Cursor::new("hello world\nrandom stuff\nworld is nice".as_bytes());
+
+        grep("world", &mut file, &|m| results.push(m));
+
+        assert_eq!(results, vec![
+            Match {
+                line: String::from("hello world"),
+                hit: (6, 11),
+            },
+            Match {
+                line: String::from("world is nice"),
+                hit: (0, 5),
+            },
+        ])
+    }
+}
+
